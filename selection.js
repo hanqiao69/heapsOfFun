e0d3d6f91a8bc2ef5615c80990bd0c8bb909bd29
Grace Huang <grace732@gmail.com>
2016-01-23 02:06:36 -0500

    connect to javascript
    connected to selection.js

--- selection.js
+++ selection.js
@@ -1,8 +1,19 @@
 /*extracts selected words when you press enter key*/
-$(document).ready(function() {
+function enter() {
   $(this).keypress(function(e){
-      if(e.which==015){
-          $selection = window.getSelection();
-      }
+      	if(e.which==015){
+        	$selection = window.getSelection();
+      		$toAdd = $selection.toString();
+      		console.log($toAdd);
+      		$(".nicEdit-main").append($toAdd);
+      		$(".nicEdit-main").append("<br>");
+      	}
     });
-});
+}
+
+function clear() {
+	$(".nicEdit-main").empty();
+	$("#NicEdit").empty();
+
+}
+
