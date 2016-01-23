cf7258ffa3bb8243772f764c1c1723f4b2f31839
Grace Huang <grace732@gmail.com>
2016-01-23 11:04:12 -0500

    Will only add to outline if selected text

--- selection.js
+++ selection.js
@@ -3,35 +3,37 @@ function enter() {
 	$times = 0;
   $(this).keypress(function(e){
       	if(e.which===015){
-
-        	$selection = window.getSelection();
+  			$selection = window.getSelection();
       		$toAdd = $selection.toString();
-      		console.log($toAdd);
-      		if ($times === 0) {
-      			$(".nicEdit-main").children().remove();
-      			$(".nicEdit-main").append("<ul id = \"startList\"> <li>" +$toAdd + "</li></ul>");
-      			$(".nicEdit-main").append("<br>");
+      		if ($toAdd) {
+	      		console.log($toAdd);
+	      		if ($times === 0) {
+	      			$(".nicEdit-main").children().remove();
+	      			$(".nicEdit-main").append("<ul id = \"startList\"> <li>" +$toAdd + "</li></ul>");
+	      			$(".nicEdit-main").append("<br>");
+	      		}
+	      		else {
+	      			$("#startList").append("<li>" +$toAdd + "</li>");
+	      		}
+	      		$times = 1;
       		}
-      		else {
-      			$("#startList").append("<li>" +$toAdd + "</li>");
-      		}
-      		$times = 1;
-      		
 
       	}
     });
 }
-/*not working yet*/
+/*not working yet
 function tab() {
-  $times = 0;
-  $(this).keypress(function(e){
-      	if(e.which===011){
-      		console.log("tab");
-      		$(".nicEdit-main").append("<ul> </ul>");
-      		
-      	}
-    });
-}
+	if($("div.nicEdit-main").is(':focus')) {
+		console.log("llala");
+	  	$(this).keypress(function(e){
+	  		console.log(e.which);
+	      	if(e.which===39){
+	      		console.log("tab");
+	      		$(".nicEdit-main").append("<ul> </ul>");
+	      	}
+	    });
+	}
+}*/
 
 function clear() {
 	$(" .nicEdit-main").empty();
@@ -44,3 +46,94 @@ function submitText() {
 	$("#input-text").before($textVal);
 	$(".mdl-textfield__input").empty();
 }
+
+ // Your Client ID can be retrieved from your project in the Google
+  // Developer Console, https://console.developers.google.com
+  var CLIENT_ID = '<628928302615-6bsd3as8err1mf4684dbf3pdhsl989jb.apps.googleusercontent.com>';
+
+  var SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+
+  /**
+   * Check if current user has authorized this application.
+   */
+  function checkAuth() {
+    gapi.auth.authorize(
+      {
+        'client_id': CLIENT_ID,
+        'scope': SCOPES.join(' '),
+        'immediate': true
+      }, handleAuthResult);
+  }
+
+  /**
+   * Handle response from authorization server.
+   *
+   * @param {Object} authResult Authorization result.
+   */
+  function handleAuthResult(authResult) {
+    var authorizeDiv = document.getElementById('authorize-div');
+    if (authResult && !authResult.error) {
+      // Hide auth UI, then load client library.
+      authorizeDiv.style.display = 'none';
+      loadDriveApi();
+    } else {
+      // Show auth UI, allowing the user to initiate authorization by
+      // clicking authorize button.
+      authorizeDiv.style.display = 'inline';
+    }
+  }
+
+  /**
+   * Initiate auth flow in response to user clicking authorize button.
+   *
+   * @param {Event} event Button click event.
+   */
+  function handleAuthClick(event) {
+    gapi.auth.authorize(
+      {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+      handleAuthResult);
+    return false;
+  }
+
+  /**
+   * Load Drive API client library.
+   */
+  function loadDriveApi() {
+    gapi.client.load('drive', 'v3', listFiles);
+  }
+
+  /**
+   * Print files.
+   */
+  function listFiles() {
+    var request = gapi.client.drive.files.list({
+        'pageSize': 10,
+        'fields': "nextPageToken, files(id, name)"
+      });
+
+      request.execute(function(resp) {
+        appendPre('Files:');
+        var files = resp.files;
+        if (files && files.length > 0) {
+          for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            appendPre(file.name + ' (' + file.id + ')');
+          }
+        } else {
+          appendPre('No files found.');
+        }
+      });
+  }
+
+  /**
+   * Append a pre element to the body containing the given message
+   * as its text node.
+   *
+   * @param {string} message Text to be placed in pre element.
+   */
+  function appendPre(message) {
+    var pre = document.getElementById('output');
+    var textContent = document.createTextNode(message + '\n');
+    pre.appendChild(textContent);
+  }
+
