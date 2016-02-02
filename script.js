7f351b4e4ea11b10b41b2021de5306b45bd3d69c
karenher <karenher@seas.upenn.edu>
2016-02-01 22:27:51 -0500

    took out unnecessary variables

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-/*Handle requests from background.html*/
+
 function handleRequest(
-	//The object data with the request params
 	request, 
-	//These last two ones isn't important for this example, if you want know more about it visit: http://code.google.com/chrome/extensions/messaging.html
-	sender, sendResponse
 	) {
 	if (request.callFunction == "toggleSidebar")
 		toggleSidebar();
